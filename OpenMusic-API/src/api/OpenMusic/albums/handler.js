5d796ee49e8a29784e33fdb7a4aa88c7c36adf76
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-12 18:46:49 +0700

    editing postAlbumHandler and getAlbumByIdHandler

--- OpenMusic-API/src/api/OpenMusic/albums/handler.js
+++ OpenMusic-API/src/api/OpenMusic/albums/handler.js
@@ -11,36 +11,25 @@ class AlbumsHandler {
 
   async postAlbumHandler(request, h) {
     this._validator.validateAlbumPayload(request.payload);
-    const { name, year } = request.payload;
 
-    const albumId = await this._service.addAlbum({
-      name, year,
-    });
+    const albumId = await this._service.addAlbum(request.payload);
 
-    const response = h.response({
+    return h.response({
       status: 'success',
       data: {
         albumId,
       },
-    });
-    response.code(201);
-    return response;
+    }).code(201);
   }
 
   async getAlbumByIdHandler(request) {
     const { id } = request.params;
     const album = await this._service.getAlbumById(id);
-    console.log(album);
-    const songs = await this._service.getSongsPerAlbum(id);
+    album.songs = await this._service.getSongsPerAlbum(id);
     return {
       status: 'success',
       data: {
-        album: {
-          ...album,
-          songs: [
-            ...songs,
-          ],
-        },
+        album,
       },
     };
   }
