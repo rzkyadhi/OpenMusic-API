8dbe27c2add505e203e86b5784a29bc5baf448f0
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-12 08:13:42 +0700

    Documentation for songs and editing on migration

--- OpenMusic-API/migrations/1650209092406_create-foreign-key-albumId-from-table-songs.js
+++ OpenMusic-API/migrations/1650209092406_create-foreign-key-albumId-from-table-songs.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 exports.up = (pgm) => {
   pgm.addConstraint('songs', 'fk_songs.albumId_albums.id', 'FOREIGN KEY("albumId") references albums(id) ON DELETE CASCADE');
 };
--- OpenMusic-API/src/api/OpenMusic/songs/routes.js
+++ OpenMusic-API/src/api/OpenMusic/songs/routes.js
@@ -1,15 +1,21 @@
+const Joi = require('joi');
+
 const routes = (handler) => [
   {
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
+    options: {
+      description: 'Post a song',
+      notes: 'Post a song to database',
+      tags: ['api'],
+    },
   },
   {
     method: 'GET',
     path: '/songs',
-    // handler: handler.getSongsHandler,
+    handler: handler.getSongsHandler,
     options: {
-      handler: handler.getSongsHandler,
       description: 'Get all songs',
       notes: 'Returns all songs in the database',
       tags: ['api'],
@@ -19,16 +25,55 @@ const routes = (handler) => [
     method: 'GET',
     path: '/songs/{id}',
     handler: handler.getSongByIdHandler,
+    options: {
+      description: 'Get a song by an ID',
+      notes: 'Returns a song by the ID passed in the path',
+      tags: ['api'],
+      validate: {
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the song'),
+        }),
+      },
+    },
   },
   {
     method: 'PUT',
     path: '/songs/{id}',
     handler: handler.putSongByIdHandler,
+    options: {
+      description: 'Edit song by an ID',
+      notes: 'Edit song details by an ID from database',
+      tags: ['api'],
+      validate: {
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the song'),
+        }),
+      },
+    },
   },
   {
     method: 'DELETE',
     path: '/songs/{id}',
     handler: handler.deleteSongByIdHandler,
+    options: {
+      description: 'delete song by an ID',
+      notes: 'Delete song by an ID from database',
+      tags: ['api'],
+      validate: {
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the song'),
+        }),
+      },
+    },
   },
 ];
 
