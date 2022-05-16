a06089ed04571cc38bc64ba538088c30caa53b73
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-16 09:34:02 +0700

    Documentation for authentication route

--- OpenMusic-API/src/api/OpenMusic/authentications/routes.js
+++ OpenMusic-API/src/api/OpenMusic/authentications/routes.js
@@ -3,16 +3,31 @@ const routes = (handler) => [
     method: 'POST',
     path: '/authentications',
     handler: handler.postAuthenticationHandler,
+    options: {
+      description: 'Post an authentication',
+      notes: 'Post an authentication to database',
+      tags: ['api'],
+    },
   },
   {
     method: 'PUT',
     path: '/authentications',
     handler: handler.putAuthenticationHandler,
+    options: {
+      description: 'Edit an authentication',
+      notes: 'Edit an authentication from database',
+      tags: ['api'],
+    },
   },
   {
     method: 'DELETE',
     path: '/authentications',
     handler: handler.deleteAuthenticationHandler,
+    options: {
+      description: 'Delete an authentication',
+      notes: 'Delete an authentication from database',
+      tags: ['api'],
+    },
   },
 ];
 
