f08a319405016badb7c431ba4c16c75b5c57a124
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-17 10:55:53 +0700

    documentation for playlistsongs route

--- OpenMusic-API/src/api/OpenMusic/playlistsongs/routes.js
+++ OpenMusic-API/src/api/OpenMusic/playlistsongs/routes.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const routes = (handler) => [
   {
     method: 'POST',
@@ -5,6 +7,34 @@ const routes = (handler) => [
     handler: handler.postPlaylistSongHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Post a song to a playlist in database',
+      notes: 'Need a token to access this route and the playlist ID passed on the path',
+      plugins: {
+        'hapi-swagger': {
+          responses: {
+            201: {
+              description: 'Lagu berhasil ditambahkan ke playlist',
+            },
+          },
+        },
+      },
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string()
+            .required()
+            .description('bearer token'),
+        }).unknown(),
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the playlist'),
+        }),
+        payload: Joi.object({
+          songId: Joi.string().required(),
+        }),
+      },
+      tags: ['api'],
     },
   },
   {
@@ -13,6 +43,31 @@ const routes = (handler) => [
     handler: handler.getPlaylistSongsHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Get a song in a playlist by an playlist ID',
+      notes: 'Need a token to access this route and the playlist ID passed on the path',
+      plugins: {
+        'hapi-swagger': {
+          responses: {
+            200: {
+              description: 'Berhasil mengambil lagu dari playlist',
+            },
+          },
+        },
+      },
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string()
+            .required()
+            .description('bearer token'),
+        }).unknown(),
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the playlist'),
+        }),
+      },
+      tags: ['api'],
     },
   },
   {
@@ -21,6 +76,31 @@ const routes = (handler) => [
     handler: handler.deletePlaylistSongHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Delete a song in a playlist',
+      notes: 'Need a token to access this route and the playlist ID passed on the path',
+      plugins: {
+        'hapi-swagger': {
+          responses: {
+            200: {
+              description: 'Berhasil menghapus lagu dari playlist',
+            },
+          },
+        },
+      },
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string()
+            .required()
+            .description('bearer token'),
+        }),
+        params: Joi.object({
+          id: Joi.string()
+            .max(50)
+            .required()
+            .description('the id for the playlist'),
+        }),
+      },
+      tags: ['api'],
     },
   },
   {
@@ -29,6 +109,8 @@ const routes = (handler) => [
     handler: handler.getPlaylistSongActivitiesHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Get playlist song activities by an playlist ID',
+      notes: 'Need a token to access this route and the playlist ID passed on the path',
     },
   },
 ];
