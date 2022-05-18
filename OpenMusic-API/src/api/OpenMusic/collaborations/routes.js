2ac0a83c70eca32f747490d42e04d508ac05467e
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-18 09:26:35 +0700

    documentation for collaborations route

--- OpenMusic-API/src/api/OpenMusic/collaborations/routes.js
+++ OpenMusic-API/src/api/OpenMusic/collaborations/routes.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const routes = (handler) => [
   {
     method: 'POST',
@@ -5,6 +7,36 @@ const routes = (handler) => [
     handler: handler.postCollaborationHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Post a collaborations to database',
+      notes: 'Need a token to access this route, also playlistId and userId in the payload',
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string()
+            .required()
+            .description('Bearer Token Type'),
+        }).unknown(),
+        payload: Joi.object({
+          playlistId: Joi.string().required(),
+          userId: Joi.string().required(),
+        }),
+      },
+      plugins: {
+        'hapi-swagger': {
+          responses: {
+            201: {
+              description: 'Created',
+              schema: Joi.object({
+                status: Joi.string().default('success'),
+                message: Joi.string().default('Kolaborasi berhasil ditambahkan'),
+                data: Joi.object({
+                  collaborationId: Joi.string().default('collab-pgXBCjxepsCxrMif'),
+                }),
+              }),
+            },
+          },
+        },
+      },
+      tags: ['api'],
     },
   },
   {
@@ -13,6 +45,33 @@ const routes = (handler) => [
     handler: handler.deleteCollaborationHandler,
     options: {
       auth: 'playlistapp_jwt',
+      description: 'Delete a collaboration',
+      notes: 'Need a token to access this route, also playlistId and userId in the payload',
+      validate: {
+        headers: Joi.object({
+          authorization: Joi.string()
+            .required()
+            .description('Bearer Token Type'),
+        }),
+        payload: Joi.object({
+          playlistId: Joi.string().required(),
+          userId: Joi.string().required(),
+        }),
+      },
+      plugins: {
+        'hapi-swagger': {
+          responses: {
+            200: {
+              description: 'OK',
+              schema: Joi.object({
+                status: Joi.string().default('success'),
+                message: Joi.string().default('Kolaborasi berhasil dihapus'),
+              }),
+            },
+          },
+        },
+      },
+      tags: ['api'],
     },
   },
 ];
