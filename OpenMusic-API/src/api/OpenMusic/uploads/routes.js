7b8cb3f0d6c14443d97619ca2f55f88d7b4a6590
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-18 10:31:04 +0700

    Documentation for upload album cover route

--- OpenMusic-API/src/api/OpenMusic/uploads/routes.js
+++ OpenMusic-API/src/api/OpenMusic/uploads/routes.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const path = require('path');
 
 const routes = (handler) => [
@@ -12,6 +13,38 @@ const routes = (handler) => [
         output: 'stream',
         maxBytes: 512000,
       },
+      description: 'Post an album cover',
+      notes: 'Need an images with max 0.512 mb',
+      plugins: {
+        'hapi-swagger': {
+          payloadType: 'multipart/form-data',
+          validate: {
+            params: Joi.object({
+              id: Joi.string()
+                .max(50)
+                .required()
+                .description('the id for the album'),
+            }),
+            payload: Joi.object({
+              file: Joi.any()
+                .meta({ swaggerType: 'file' })
+                .required()
+                .description('image file'),
+            }),
+          },
+          responses: {
+            201: {
+              description: 'Created',
+              content: 'multipart/form-data',
+              schema: Joi.object({
+                status: Joi.string().default('success'),
+                message: Joi.string().default('Sampul berhasil diunggah'),
+              }),
+            },
+          },
+        },
+      },
+      tags: ['api'],
     },
   },
   {
