3c9c65f50d1337ba3d48aa3cc0625dde310c9017
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-05-18 20:10:41 +0700

    UPDATE documentation for users route

--- OpenMusic-API/src/api/OpenMusic/users/routes.js
+++ OpenMusic-API/src/api/OpenMusic/users/routes.js
@@ -1,3 +1,7 @@
+const Joi = require('joi');
+
+const currentTime = new Date().getTime();
+
 const routes = (handler) => [
   {
     method: 'POST',
@@ -7,6 +11,29 @@ const routes = (handler) => [
       description: 'Post an user',
       notes: 'Post an user to database',
       tags: ['api'],
+      plugins: {
+        'hapi-swagger': {
+          validate: {
+            payload: Joi.object({
+              username: Joi.string().max(50).required().default(`dicoding_${currentTime}`),
+              password: Joi.string().required().default('secret'),
+              fullname: Joi.string().required().default('Dicoding Indonesia'),
+            }),
+          },
+          responses: {
+            201: {
+              description: 'Created',
+              schema: Joi.object({
+                status: 'success',
+                message: 'User berhasil ditambahkan',
+                data: {
+                  userId: 'user-lz1KPr2bf8PmEIsV',
+                },
+              }),
+            },
+          },
+        },
+      },
     },
   },
 ];
